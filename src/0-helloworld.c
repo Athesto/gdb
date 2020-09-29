0304a94b87ff63b15b2b0d64672b1997d8110456
Athesto <gamez.live@gmail.com>
2020-09-29 19:26:16 +0000

    0-helloworld.c

---
+++ src/0-helloworld.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - demo1
+ */
+int main(void)
+{
+	int a = 10;
+	int b = 100;
+	int c = 140;
+	int answer;
+	printf(" %3d\n", a);
+	printf(" %3d\n", b);
+	printf("+%3d\n", c);
+	printf("-----\n");
+	answer = c;
+	printf(" %3d\n", answer);
+
+	return (0);
+}
